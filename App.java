698be7a99bd944e3f56432e08a462a80530988ee
Boomer-git <83802658+Boomer-git@users.noreply.github.com>
2025-08-26 18:12:49 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,16 @@
+import java.io.*;
+import java.net.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java App in Docker!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(80);
+        System.out.println("Server running on port 80...");
+        while (true) {
+            Socket client = serverSocket.accept();
+            OutputStream out = client.getOutputStream();
+            out.write("HTTP/1.1 200 OK\r\n\r\nHello from Java App".getBytes());
+            out.flush();
+            client.close();
+        }
     }
 }
